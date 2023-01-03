742ab2c43996a6a84f69a0363419ceff84e1f029
umar <umar.dev500@gmail.com>
2023-01-03 19:01:12 +0700

    change color on order status list

--- components/molecules/orderListing/OrderListing.tsx
+++ components/molecules/orderListing/OrderListing.tsx
@@ -28,11 +28,11 @@ export const OrderListing = React.memo(({ index, ...props }: Props) => {
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">{name}</td>
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">{productName}</td>
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">{toCurrency(productPrice)}</td>
-            <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">
+            <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap text-left">
                 <span
-                    className={`${status === 'cancel' ? 'bg-amber-300 text-gray-500' : ''} ${status === 'pending' ? 'bg-slate-200 text-gray-500' : ''} ${
-                        status === 'settlement' ? 'bg-emerald-400 text-gray-100' : ''
-                    } py-1 px-2 rounded text-sm`}
+                    className={`${status === 'cancel' ? 'bg-amber-400 text-gray-100' : ''} ${status === 'pending' ? 'bg-indigo-300 text-gray-100' : ''} ${
+                        status === 'settlement' ? 'bg-green-400 text-gray-100' : ''
+                    } py-1 px-1.5 rounded text-sm`}
                 >
                     {toUpperFirst(status)}
                 </span>
