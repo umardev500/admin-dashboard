396a5a53410a753f67dbe5b8cd1bdec2e1aacfd4
umar <umar.dev500@gmail.com>
2023-01-03 10:24:33 +0700

    create order response

---
+++ types/orderResponse.ts
@@ -0,0 +1,22 @@
+export interface OrderUser {
+    customer_id: string
+    name: string
+    user: string
+}
+
+export interface OrderProduct {
+    product_id: string
+    name: string
+    price: number
+    duration: number
+    description: string
+}
+
+export interface OrderResponse {
+    order_id: string
+    buyer: OrderUser
+    product: OrderProduct
+    status: string
+    created_at: number
+    updated_at: number
+}
