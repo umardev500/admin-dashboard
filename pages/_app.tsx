5260e6f1cdb558b4de77bbbc3531a8bf8da1c105
umar <umar.dev500@gmail.com>
2023-01-08 11:24:42 +0700

    add main context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,19 @@
 import '../styles/app.scss'
 import type { AppProps } from 'next/app'
-import { AppProvider } from '../contexts'
+import { AppProvider, MainProvider } from '../contexts'
 import { Dashboard } from '../components'
 import { AnimatePresence } from 'framer-motion'
 
 export default function App({ Component, pageProps }: AppProps): React.ReactNode {
     return (
-        <AppProvider>
-            <Dashboard>
-                <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
-                    <Component {...pageProps} />
-                </AnimatePresence>
-            </Dashboard>
-        </AppProvider>
+        <MainProvider>
+            <AppProvider>
+                <Dashboard>
+                    <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
+                        <Component {...pageProps} />
+                    </AnimatePresence>
+                </Dashboard>
+            </AppProvider>
+        </MainProvider>
     )
 }
