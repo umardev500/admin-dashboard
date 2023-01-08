3b9881cb60c1b5d78fe47adb538bb97f9c816a36
umar <umar.dev500@gmail.com>
2023-01-08 13:13:52 +0700

    add order pending count

--- components/organisms/featuredList/FeaturedList.tsx
+++ components/organisms/featuredList/FeaturedList.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Featured } from '../../molecules'
 
-export const FeaturedList: React.FC = () => {
+interface Props {
+    customerCount: number
+    expiredCount: number
+    orderPendingCount: number
+}
+
+export const FeaturedList: React.FC<Props> = ({ customerCount, expiredCount, orderPendingCount }) => {
     return (
         <div className="flex flex-col lg:flex-row pt-5">
-            <Featured icon="url('/assets/icons/book-filled.svg')" color="#6271EB" textName="Pelanggan" textValue={10} />
+            <Featured icon="url('/assets/icons/book-filled.svg')" color="#6271EB" textName="Pelanggan" textValue={customerCount} />
             <Featured icon="url('/assets/icons/receipt-filled.svg')" color="#059669" textName="Pendaftaran" textValue={10} />
-            <Featured icon="url('/assets/icons/refresh.svg')" color="#FB923C" textName="Kadaluarsa" textValue={10} />
-            <Featured icon="url('/assets/icons/task-featured.svg')" color="#DC3545" textName="Pending" textValue={10} />
+            <Featured icon="url('/assets/icons/refresh.svg')" color="#FB923C" textName="Kadaluarsa" textValue={expiredCount} />
+            <Featured icon="url('/assets/icons/task-featured.svg')" color="#DC3545" textName="Order Pending" textValue={orderPendingCount} />
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,66 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import { FeaturedList } from '../components'
+import { CustomerResponse } from '../types'
+
+const MEMBERSHIP_API = process.env.MEMBERSHIP_API as string
 
 export default function Home(): React.ReactNode {
+    const [customerCount, setCustomerCount] = useState(0)
+    const [expiredCount, setExpiredCount] = useState(0)
+    const [orderPendingCount, setOrderPendingCount] = useState(0)
+
+    const fetchData = async (route: string): Promise<number> => {
+        // localhost:8000/membership/api/customers?status=none&count_only=true
+        const target = `${MEMBERSHIP_API}${route}`
+        const response = await fetch(target)
+        const data: CustomerResponse = await response.json()
+
+        if (data.status_code !== 200) return await Promise.reject(data.message)
+        const customersData = data.data
+        const rows = customersData.rows
+        if (rows !== undefined) {
+            return await Promise.resolve(rows)
+        }
+
+        // if undefined or no data
+        return await Promise.resolve(0)
+    }
+
+    // Fetch customer count
+    useEffect(() => {
+        fetchData('/customers?status=none&count_only=true')
+            .then((rows) => {
+                setCustomerCount(rows)
+            })
+            .catch((err) => {
+                console.log(err)
+                setCustomerCount(0)
+            })
+        fetchData('/customers?status=expired&count_only=true')
+            .then((rows) => {
+                setExpiredCount(rows)
+            })
+            .catch((err) => {
+                console.log(err)
+                setExpiredCount(0)
+            })
+        fetchData('/orders?status=pending&count_only=true')
+            .then((rows) => {
+                setOrderPendingCount(rows)
+            })
+            .catch((err) => {
+                console.log(err)
+                setOrderPendingCount(0)
+            })
+    }, [])
+
     return (
         <>
             <Head>
                 <title>Dashboard</title>
             </Head>
-            <FeaturedList />
+            <FeaturedList customerCount={customerCount} expiredCount={expiredCount} orderPendingCount={orderPendingCount} />
         </>
     )
 }
