ef5bb33b148aa6b9caeb809326ed9935a9a6cfe7
umar <umar.dev500@gmail.com>
2023-01-09 23:45:27 +0700

    chcek for is auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,25 @@ import { AppProvider, MainProvider } from '../contexts'
 import { Dashboard } from '../components'
 import { AnimatePresence } from 'framer-motion'
 
-export default function App({ Component, pageProps }: AppProps): React.ReactNode {
+export default function App({ Component, pageProps, router }: AppProps): React.ReactNode {
+    const { pathname } = router
+
+    const noAuth = ['/auth', '/register']
+
+    const isOk = noAuth.find((val) => val === pathname)
+
     return (
         <MainProvider>
             <AppProvider>
-                <Dashboard>
-                    <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
-                        <Component {...pageProps} />
-                    </AnimatePresence>
-                </Dashboard>
+                {isOk === undefined ? (
+                    <Dashboard>
+                        <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
+                            <Component {...pageProps} />
+                        </AnimatePresence>
+                    </Dashboard>
+                ) : (
+                    <Component {...pageProps} />
+                )}
             </AppProvider>
         </MainProvider>
     )
---
+++ pages/auth/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const Auth: NextPage = () => {
+    return <p>Hello</p>
+}
+
+export default Auth
