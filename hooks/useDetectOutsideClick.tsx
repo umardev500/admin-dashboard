032b18e70612f62319c8a436eb4c0f17117228fe
umar <umar.dev500@gmail.com>
2023-01-10 18:00:15 +0700

    remove unused params

--- hooks/useDetectOutsideClick.tsx
+++ hooks/useDetectOutsideClick.tsx
@@ -4,19 +4,16 @@ interface Props {
     parent: React.RefObject<HTMLElement>
     target: React.RefObject<HTMLElement>
     status: React.Dispatch<React.SetStateAction<boolean>>
-    setOutsideClicked?: React.Dispatch<React.SetStateAction<boolean>>
     toActivate?: boolean
 }
 
-export const useDetectOutsideClick = ({ parent, target, status, setOutsideClicked, toActivate }: Props): void => {
+export const useDetectOutsideClick = ({ parent, target, status, toActivate }: Props): void => {
     useEffect(() => {
         const handleClick = (e: MouseEvent): void => {
             const isInner = target.current?.contains(e.target as Node)
-            console.log('click again here')
             // if (toActivate === true && isInner === true) status(true)
 
             if (isInner !== undefined && !isInner) {
-                if (setOutsideClicked != null) setOutsideClicked(true)
                 status(isInner as boolean)
             }
         }
