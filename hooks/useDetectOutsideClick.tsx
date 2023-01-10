1f8080dbe7e89282098a971692883a2c2a3be8ac
umar <umar.dev500@gmail.com>
2023-01-10 17:59:28 +0700

    fix not remove listener

--- hooks/useDetectOutsideClick.tsx
+++ hooks/useDetectOutsideClick.tsx
@@ -1,4 +1,3 @@
-import { removeAllListeners } from 'process'
 import React, { useEffect } from 'react'
 
 interface Props {
@@ -6,21 +5,29 @@ interface Props {
     target: React.RefObject<HTMLElement>
     status: React.Dispatch<React.SetStateAction<boolean>>
     setOutsideClicked?: React.Dispatch<React.SetStateAction<boolean>>
+    toActivate?: boolean
 }
 
-export const useDetectOutsideClick = ({ parent, target, status, setOutsideClicked }: Props): void => {
+export const useDetectOutsideClick = ({ parent, target, status, setOutsideClicked, toActivate }: Props): void => {
     useEffect(() => {
-        const ev = parent.current?.addEventListener('click', (e) => {
+        const handleClick = (e: MouseEvent): void => {
             const isInner = target.current?.contains(e.target as Node)
+            console.log('click again here')
+            // if (toActivate === true && isInner === true) status(true)
 
             if (isInner !== undefined && !isInner) {
                 if (setOutsideClicked != null) setOutsideClicked(true)
                 status(isInner as boolean)
             }
-        })
+        }
+
+        const element = parent.current
+        if (element !== undefined && element !== null) {
+            element.addEventListener('click', handleClick)
+        }
 
         return () => {
-            removeAllListeners(ev as any)
+            if (element !== undefined && element !== null) element.removeEventListener('click', handleClick)
         }
     }, [])
 }
