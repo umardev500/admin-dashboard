6c21cc730083c6c27ae9b80b28238a9dda288baa
umar <umar.dev500@gmail.com>
2023-01-12 13:52:00 +0700

    add redirect if already loggedin

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,28 @@ import { NextRequest, NextResponse } from 'next/server'
 import { auth } from './middlewares/auth'
 
 export async function middleware(req: NextRequest): Promise<NextResponse> {
-    if (req.nextUrl.pathname.startsWith('/products')) {
-        const href = req.nextUrl.href
-        try {
-            await auth(req)
-        } catch (err) {
-            return NextResponse.redirect(new URL(`/auth?redirect=${href}`, req.url))
+    const pathname = req.nextUrl.pathname
+
+    const isNext = pathname.startsWith('/_next')
+    const isAssets = pathname.startsWith('/assets')
+
+    if (!isNext && !isAssets) {
+        if (!req.nextUrl.pathname.startsWith('/auth')) {
+            const href = req.nextUrl.href
+            try {
+                await auth(req)
+            } catch (err) {
+                return NextResponse.redirect(new URL(`/app/auth?redirect=${href}`, req.url))
+            }
+        }
+
+        if (req.nextUrl.pathname.startsWith('/auth')) {
+            try {
+                await auth(req)
+                return NextResponse.redirect(new URL('/app', req.url))
+            } catch (err) {
+                console.log(err)
+            }
         }
     }
 
