a35980c2d360e73dcf936cf039a55fc16bfa935e
umar <umar.dev500@gmail.com>
2023-01-12 15:10:25 +0700

    fix icon not loaded

--- components/organisms/featuredList/FeaturedList.tsx
+++ components/organisms/featuredList/FeaturedList.tsx
@@ -10,10 +10,10 @@ interface Props {
 export const FeaturedList: React.FC<Props> = ({ customerCount, expiredCount, orderPendingCount }) => {
     return (
         <div className="flex flex-col lg:flex-row pt-5">
-            <Featured icon="url('/assets/icons/book-filled.svg')" color="#6271EB" textName="Pelanggan" textValue={customerCount} />
-            <Featured icon="url('/assets/icons/receipt-filled.svg')" color="#059669" textName="Pendaftaran" textValue={10} />
-            <Featured icon="url('/assets/icons/refresh.svg')" color="#FB923C" textName="Kadaluarsa" textValue={expiredCount} />
-            <Featured icon="url('/assets/icons/task-featured.svg')" color="#DC3545" textName="Order Pending" textValue={orderPendingCount} />
+            <Featured icon="url('/app/assets/icons/book-filled.svg')" color="#6271EB" textName="Pelanggan" textValue={customerCount} />
+            <Featured icon="url('/app/assets/icons/receipt-filled.svg')" color="#059669" textName="Pendaftaran" textValue={10} />
+            <Featured icon="url('/app/assets/icons/refresh.svg')" color="#FB923C" textName="Kadaluarsa" textValue={expiredCount} />
+            <Featured icon="url('/app/assets/icons/task-featured.svg')" color="#DC3545" textName="Order Pending" textValue={orderPendingCount} />
         </div>
     )
 }
