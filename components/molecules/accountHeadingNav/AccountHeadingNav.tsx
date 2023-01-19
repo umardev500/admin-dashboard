0c26709d530ffb506bb3bc0a1c2c7335afeedbeb
umar <umar.dev500@gmail.com>
2023-01-19 18:08:07 +0700

    change color for heading nav

--- components/molecules/accountHeadingNav/AccountHeadingNav.tsx
+++ components/molecules/accountHeadingNav/AccountHeadingNav.tsx
@@ -4,13 +4,18 @@ export const AccountHeadingNav: React.FC = () => {
     return (
         <ul className="flex gap-x-4">
             <li>
-                <a href="#" className="outline-none inline-flex items-center justify-center roboto font-medium text-gray-500 px-4 h-10 border-b-2 border-b-gray-400">
+                <a href="#" className="outline-none inline-flex items-center justify-center roboto font-medium text-gray-600 px-4 h-10 border-b-2 border-b-gray-500">
                     Overview
                 </a>
             </li>
             <li>
                 <a href="#" className="outline-none inline-flex items-center justify-center roboto font-medium text-gray-500 px-4 h-10">
                     Earnings
+                </a>
+            </li>
+            <li>
+                <a href="#" className="outline-none inline-flex items-center justify-center roboto font-medium text-gray-500 px-4 h-10">
+                    Profiles
                 </a>
             </li>
         </ul>
