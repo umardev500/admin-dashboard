2dbd9daf0e2d33ab5c652b55816f290f6ef5e00b
umar <umar.dev500@gmail.com>
2023-01-19 18:34:08 +0700

    prepare for main page or overview

--- components/molecules/overviewEarning/OverViewEarning.tsx
+++ components/molecules/overviewEarning/OverViewEarning.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 import React from 'react'
 
 interface Props {
-    isPage: boolean
+    isPage?: boolean
 }
 
 export const OverviewEarning: React.FC<Props> = ({ isPage }) => {
     return (
         <div className="bg-white flex flex-1 flex-col rounded-xl px-6 py-5">
-            <div className="flex items-center justify-between roboto font-medium text-lg text-gray-500">
-                {isPage ? (
+            <div className={`flex items-center ${isPage !== true ? 'justify-between' : 'gap-5'} roboto font-medium text-lg text-gray-500`}>
+                {isPage === true ? (
                     <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M8.77234 5.43585L3.20817 11L8.77234 16.5642M18.7915 11H3.364"
@@ -22,7 +22,7 @@ export const OverviewEarning: React.FC<Props> = ({ isPage }) => {
                     </svg>
                 ) : null}
                 <span>Earnings</span>
-                {!isPage ? (
+                {isPage !== true ? (
                     <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M13.7274 5.43582L19.2916 11L13.7274 16.5642M3.70825 11H19.1358"
