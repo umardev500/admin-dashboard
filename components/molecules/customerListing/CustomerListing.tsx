78e043d0daaf71c9fdbf2ca1d912c456512d9021
umar <umar.dev500@gmail.com>
2023-02-10 14:16:22 +0700

    use boolean to check customer status class

--- components/molecules/customerListing/CustomerListing.tsx
+++ components/molecules/customerListing/CustomerListing.tsx
@@ -36,7 +36,7 @@ export const CustomerListing: React.FC<Props> = ({ index, serverTime, ...props }
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">{email}</td>
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">{parseDate(createdTime)}</td>
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">
-                <span className={`${getCustomerStatusClass(status, expiredTime)} text-gray-100 px-1.5 py-1 rounded text-sm`}>
+                <span className={`${getCustomerStatusClass(status, isExpired)} text-gray-100 px-1.5 py-1 rounded text-sm`}>
                     {toUpperFirst(isExpired || status === 'expired' ? 'Expired' : status)}
                 </span>
             </td>
--- helpers/getCustomerStatusClass.ts
+++ helpers/getCustomerStatusClass.ts
@@ -1,5 +1,5 @@
-export const getCustomerStatusClass = (status: string, expiredTime?: number): string => {
-    if (expiredTime !== undefined || status === 'expired') return 'bg-amber-400'
+export const getCustomerStatusClass = (status: string, expiredTime?: boolean): string => {
+    if ((expiredTime !== undefined && expiredTime) || status === 'expired') return 'bg-amber-400'
     if (status === 'pending') return 'bg-indigo-300'
     if (status === 'active') return 'bg-green-400'
 
