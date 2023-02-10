55d6eecbe1967237ea557b7da9f43512f3f73f75
umar <umar.dev500@gmail.com>
2023-02-10 12:26:04 +0700

    make featured list auto wrapping

--- components/organisms/featuredList/FeaturedList.tsx
+++ components/organisms/featuredList/FeaturedList.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export const FeaturedList: React.FC<Props> = ({ customerCount, expiredCount, orderPendingCount, newMemberCount }) => {
     return (
-        <div className="flex flex-col lg:flex-row pt-5">
+        <div className="flex flex-col flex-wrap lg:flex-row pt-5">
             <Featured icon="url('/app/assets/icons/book-filled.svg')" color="#6271EB" textName="Pelanggan" textValue={customerCount} />
             <Featured icon="url('/app/assets/icons/receipt-filled.svg')" color="#3b82f6" textName="Pendaftaran" textValue={newMemberCount} />
             <Featured icon="url('/app/assets/icons/refresh.svg')" color="#FB923C" textName="Kadaluwarsa" textValue={expiredCount} />
