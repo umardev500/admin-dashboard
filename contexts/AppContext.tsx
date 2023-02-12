c3447f5213c689734f48eebdf6bba1de9cdfba79
umar <umar.dev500@gmail.com>
2023-02-12 16:56:00 +0700

    remove fetching server time from provider

--- contexts/AppContext.tsx
+++ contexts/AppContext.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useMemo, useState } from 'react'
-import { useSyncServerTime } from '../hooks'
 import { PageProps } from '../types'
 
 interface Props extends PageProps {
@@ -9,32 +8,23 @@ interface Props extends PageProps {
 export interface AppContextType extends PageProps {
     shown: boolean
     setShown: React.Dispatch<React.SetStateAction<boolean>>
-    serverTime: number
 }
 
 export const AppContext = React.createContext({})
 
 export const AppProvider: React.FC<Props> = ({ children, ...pageProps }) => {
     const [shown, setShown] = useState(false)
-    const [serverTime, setServerTime] = useState(0)
-
-    const handleSyncServerTime = useSyncServerTime()
 
     const data = useMemo<AppContextType>(() => {
         return {
             shown,
             setShown,
-            serverTime,
             ...pageProps,
         }
-    }, [shown, serverTime])
+    }, [shown])
 
     useEffect(() => {
-        handleSyncServerTime()
-            .then((res) => {
-                setServerTime(parseInt(res))
-            })
-            .catch((err) => console.log('error getting server time:', err))
+        console.log('Provider rendered')
     }, [])
 
     return <AppContext.Provider value={data}>{children}</AppContext.Provider>
