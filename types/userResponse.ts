7d9c451acdf5741d875a01fe530673a1b2e591bd
umar <umar.dev500@gmail.com>
2023-02-12 09:33:06 +0700

    add user response type

---
+++ types/userResponse.ts
@@ -0,0 +1,38 @@
+import { BasicAPIResponse } from './basicAPIResponse'
+
+export interface UserLocation {
+    address: string
+    village: string
+    district: string
+    city: string
+    province: string
+    postal_code: string
+}
+
+export interface UserDetail {
+    name: string
+    email: string
+    phone: string
+    location: UserLocation
+}
+
+export interface User {
+    user_id: string
+    user: string
+    pass: string
+    detail: UserDetail
+    level: string
+    status: string
+    created_at: number
+    update_at: number
+    deleted_at: number
+}
+
+export interface UserData {
+    is_empty: boolean
+    payload: User
+}
+
+export interface UserResponse extends BasicAPIResponse {
+    data: UserData
+}
