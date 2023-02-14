7905a235911a32c11156edbf9b341f141f3a2cf2
umar <umar.dev500@gmail.com>
2023-02-14 20:56:41 +0700

    fix typo

--- components/molecules/profileMenu/ProfileMenu.tsx
+++ components/molecules/profileMenu/ProfileMenu.tsx
@@ -43,9 +43,9 @@ export const ProfileMenu: React.FC = () => {
                     <span className="ml-3">Alamat</span>
                 </Link>
                 <Link
-                    href={'/settings/account/profiles/auhtentication'}
+                    href={'/settings/account/profiles/authentication'}
                     className={`roboto whitespace-nowrap border-l-4 ${getClasses([
-                        '/settings/account/profiles/auhtentication',
+                        '/settings/account/profiles/authentication',
                     ])} py-3 flex items-center px-5 text-gray-500 hover:text-gray-400 font-medium`}
                 >
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
