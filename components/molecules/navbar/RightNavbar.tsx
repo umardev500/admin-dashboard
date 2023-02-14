a58a4ee3baa7abc38e58f21c2ac6b739cac17900
umar <umar.dev500@gmail.com>
2023-02-14 09:22:36 +0700

    use avatar from data

--- components/molecules/navbar/RightNavbar.tsx
+++ components/molecules/navbar/RightNavbar.tsx
@@ -1,10 +1,15 @@
-import React, { useRef, useState } from 'react'
 import Image from 'next/image'
+import React, { useContext, useRef, useState } from 'react'
+import { AppContext, AppContextType } from '../../../contexts'
+import { imgLoader } from '../../../helpers'
 import { Dropdown } from '../dropdown'
 
 export const RightNavbar: React.FC = () => {
     const [avatarMenu, setAvatarMenu] = useState<boolean>(false)
     const navRef = useRef<HTMLUListElement>(null)
+    const ctx = useContext(AppContext) as AppContextType
+    const userData = ctx.userData
+    const avatar = userData?.detail.avatar ?? 'avatar.png'
 
     const onDropdownToggle = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, setDropdownToggle: React.Dispatch<React.SetStateAction<boolean>>): void => {
         e.preventDefault()
@@ -17,7 +22,7 @@ export const RightNavbar: React.FC = () => {
         <ul className="navigation flex" ref={navRef}>
             <li className="flex relative">
                 <a onClick={(e) => onDropdownToggle(e, setAvatarMenu)} className="navigation-link avatar-item flex items-center px-4" href="#">
-                    <Image src={'/app/assets/avatars/avatar.png'} width={38} height={38} alt="avatar" quality={100} priority />
+                    <Image loader={imgLoader} src={avatar} width={38} height={38} alt="avatar" quality={100} priority />
                 </a>
                 {avatarMenu ? <Dropdown setState={setAvatarMenu} right /> : null}
             </li>
