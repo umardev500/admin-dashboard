86543f3eaf7d70c8b81b75e0a634010505e2c71d
umar <umar.dev500@gmail.com>
2023-02-15 00:57:13 +0700

    use ref

--- components/atoms/input/Input.tsx
+++ components/atoms/input/Input.tsx
@@ -10,24 +10,29 @@ interface Props {
     defaultValue?: string
 }
 
-export const Input: React.FC<Props> = ({ title, placeholder, disabled = false, required = false, className = '', type = 'text', defaultValue }) => {
-    let disabledClasses = 'bg-gray-100'
-    if (!disabled) disabledClasses = ''
+export const Input: React.FC<Props> = React.forwardRef(
+    ({ title, placeholder, disabled = false, required = false, className = '', type = 'text', defaultValue }: Props, ref?: React.LegacyRef<HTMLInputElement>) => {
+        let disabledClasses = 'bg-gray-100'
+        if (!disabled) disabledClasses = ''
 
-    return (
-        <div className={`flex-1 ${className}`}>
-            <div className="flex text-gray-500">
-                <span>{title}</span>
-                {required ? <span className="text-red-400">*</span> : null}
+        return (
+            <div className={`flex-1 ${className}`}>
+                <div className="flex text-gray-500">
+                    <span>{title}</span>
+                    {required ? <span className="text-red-400">*</span> : null}
+                </div>
+                <input
+                    ref={ref}
+                    type={type}
+                    disabled={disabled}
+                    required={required}
+                    defaultValue={defaultValue}
+                    className={`${disabledClasses} border text-gray-500 outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 px-4 h-12 w-full mt-2 rounded-lg`}
+                    placeholder={placeholder}
+                />
             </div>
-            <input
-                type={type}
-                disabled={disabled}
-                required={required}
-                defaultValue={defaultValue}
-                className={`${disabledClasses} border text-gray-500 outline-none focus:ring-1 focus:ring-blue-400 focus:border-blue-400 px-4 h-12 w-full mt-2 rounded-lg`}
-                placeholder={placeholder}
-            />
-        </div>
-    )
-}
+        )
+    }
+)
+
+Input.displayName = 'Input'
