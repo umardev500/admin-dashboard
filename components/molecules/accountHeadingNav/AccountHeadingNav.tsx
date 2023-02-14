4f561b0ac40805ba3f52d63e18dba38ea9161910
umar <umar.dev500@gmail.com>
2023-02-14 12:19:17 +0700

    use checkrouteClasses

--- components/molecules/accountHeadingNav/AccountHeadingNav.tsx
+++ components/molecules/accountHeadingNav/AccountHeadingNav.tsx
@@ -1,38 +1,36 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
-import { checkPath } from '../../../helpers'
+import { checkRouteClass } from '../../../helpers'
 
 export const AccountHeadingNav: React.FC = () => {
     const router = useRouter()
+    const pathName = router.pathname
 
     const basePath = '/settings/account'
-    const getClasses = (path: string[]): string => {
-        let className = 'outline-none inline-flex items-center justify-center roboto font-medium text-gray-500 hover:text-gray-600 px-4 h-10'
-        path.forEach((val) => {
-            if (checkPath(router, basePath + val)) {
-                className +=
-                    " !text-gray-600 before:absolute before:content-[''] before:bg-slate-500 before:border-b-2 before:border-gray-500 before:flex before:left-0 before:right-0 before:bottom-0"
-            }
-        })
+    const defaultClasses = 'outline-none inline-flex items-center justify-center roboto font-medium hover:text-gray-600 px-4 h-10'
 
-        return className
+    const getClasses = (paths: string[]): string => {
+        const mathedClasses =
+            "text-gray-600 before:absolute before:content-[''] before:bg-slate-500 before:border-b-2 before:border-gray-500 before:flex before:left-0 before:right-0 before:bottom-0"
+
+        return checkRouteClass(paths, pathName, mathedClasses, 'text-gray-500')
     }
 
     return (
         <ul className="flex gap-x-4 overflow-auto heading-nav">
             <li className="relative">
-                <Link href="/settings/account/" className={`${getClasses([''])} `}>
+                <Link href="/settings/account/" className={`${defaultClasses} ${getClasses([basePath])}`}>
                     Overview
                 </Link>
             </li>
             <li className="relative">
-                <Link href="/settings/account/earning" className={getClasses(['/earning'])}>
+                <Link href="/settings/account/earning" className={`${defaultClasses} ${getClasses([`${basePath}/earning`])}`}>
                     Earnings
                 </Link>
             </li>
             <li className="relative">
-                <Link href="/settings/account/profiles" className={getClasses(['/profiles'])}>
+                <Link href="/settings/account/profiles" className={`${defaultClasses} ${getClasses([`${basePath}/profiles`])}`}>
                     Profiles
                 </Link>
             </li>
