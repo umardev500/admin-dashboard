65a87bafb9bff75b08eb37bd1239306bc22436d3
umar <umar.dev500@gmail.com>
2023-02-16 12:00:53 +0700

    fix error rendered ui at first time / error hydration

--- components/template/Dashboard.tsx
+++ components/template/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence } from 'framer-motion'
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { AppContext, AppContextType } from '../../contexts'
 import { Header, Sidebar } from '../organisms'
@@ -11,6 +11,7 @@ interface Props {
 export const Dashboard: React.FC<Props> = ({ children }) => {
     const ctx = useContext(AppContext) as AppContextType
     const bodyRef = useRef<HTMLDivElement>(null)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         // showing
@@ -22,6 +23,8 @@ export const Dashboard: React.FC<Props> = ({ children }) => {
         if (!ctx.shown) {
             bodyRef.current?.classList.remove('sidebar-shown')
         }
+
+        setLoading(false)
     })
 
     return (
@@ -31,7 +34,7 @@ export const Dashboard: React.FC<Props> = ({ children }) => {
             <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
                 <div className="content">{children}</div>
             </AnimatePresence>
-            <Toaster />
+            {!loading ? <Toaster /> : null}
         </div>
     )
 }
