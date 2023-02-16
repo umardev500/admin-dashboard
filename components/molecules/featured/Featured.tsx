8a3c21be5a23d533a5c1c69937d32fc487403274
umar <umar.dev500@gmail.com>
2023-02-16 19:23:38 +0700

    now wrap featured

--- components/molecules/featured/Featured.tsx
+++ components/molecules/featured/Featured.tsx
@@ -11,7 +11,7 @@ export const Featured: React.FC<Props> = ({ textName, textValue, color, icon }) 
     return (
         <div className="featured mr-4 mb-4 flex-1 bg-white flex items-center p-4 lg:p-5">
             <div style={{ ['--icon' as string]: icon, ['--color' as string]: color }} className="icon-box mr-4"></div>
-            <div className="flex flex-col">
+            <div className="flex flex-col whitespace-nowrap">
                 <span className="featured-name text-lg text-gray-500 font-medium">{textName}</span>
                 <span className="featured-value text-gray-500 text-xl font-semibold whitespace-nowrap text-ellipsis roboto">{textValue}</span>
             </div>
