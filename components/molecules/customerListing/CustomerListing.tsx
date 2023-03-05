e9596cf1e707b2acdc04b4f041ffb07f56c7352d
umar <umar.dev500@gmail.com>
2023-03-05 12:26:24 +0700

    add getStatus for customerListing

--- components/molecules/customerListing/CustomerListing.tsx
+++ components/molecules/customerListing/CustomerListing.tsx
@@ -15,8 +15,13 @@ export const CustomerListing: React.FC<Props> = ({ index, serverTime, ...props }
     const [detailModal, setDetailModal] = useState(false)
     const [deleteModal, setDeleteModal] = useState(false)
 
-    const calculatedTime = expiredTime - serverTime
-    const isExpired = calculatedTime < 0
+    const getStatus = (): string => {
+        const calculatedTime = expiredTime - serverTime
+        const isExpired = calculatedTime < 0
+        if (!isNaN(calculatedTime) && isExpired) return 'Expired'
+
+        return toUpperFirst(status)
+    }
 
     const handleClickDelete = useCallback(() => {
         setDeleteModal(true)
@@ -35,7 +40,7 @@ export const CustomerListing: React.FC<Props> = ({ index, serverTime, ...props }
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">{email}</td>
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">{parseDate(createdTime)}</td>
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap">
-                <span className={`px-1.5 py-1 rounded`}>{toUpperFirst(isExpired || status === 'expired' ? 'Expired' : status)}</span>
+                <span className={`px-1.5 py-1 rounded`}>{getStatus()}</span>
             </td>
             <td className="px-4 border-r border-b border-slate-200 py-2 whitespace-nowrap w-10">
                 <div className="flex items-center">
